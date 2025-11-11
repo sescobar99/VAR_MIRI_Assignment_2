e2d93e6e359ba17414e733852c280d19f39dc84c
Santiago Escobar <santi.e.c@hotmail.com>
2025-11-11 16:22:41 +0100

    Allow resizable device

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # VAR_MIRI_Assignment_2
+
+
+## Desarrollo:
+
+- Instalar node
+- La instalación debería dejar disponible NPM 
+- En una terminal ejecutar `npm install` para instalar las dependencias (definidas en el package-lock.json o package.json)
+- Ejecutar en una terminal `npm run dev` para levantar el servidor de vite (se hace con este comando porque ya está configurado correctamente en el package.json )
+
+
+## Build:
+- Ejecutar en una terminal `npm run build` para generar la versión de despliegue en el directorio /docs (configurado asi para facilitar trabajo con github pages)
+
+## Despliegue
+- Github pages está configurado para actualizar la versión del proyecto según los cambios que se hagan en la rama master en el directorio /docs
--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 import * as THREE from 'three';
 
-console.log("Java script is running")
+console.log("¡Java script is running!")
 
+// Resize renderer when resizing webpage
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+
+// Initial configuration
+
+// Element 1: Scene
 const scene = new THREE.Scene();
+// Element 2: Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// FOV is given in degrees
 
+// Element 3: Renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
